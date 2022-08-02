c027322f07c8d042afb828c39dd9fa811cfb5321
jinping.he <jinping.he@opay-inc.com>
2022-08-02 17:58:39 +0800

    ceshi

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
+        System.out.println(456);
         SpringApplication.run(TestApplication.class, args);
     }
 
