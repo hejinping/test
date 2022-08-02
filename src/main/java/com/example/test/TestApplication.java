112edca8d6e98616a96d513410d09200f7138ed1
jinping.he <jinping.he@opay-inc.com>
2022-08-02 18:27:43 +0800

    ttt

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        System.out.println(456);
+        System.out.println(9999);
         SpringApplication.run(TestApplication.class, args);
     }
 
